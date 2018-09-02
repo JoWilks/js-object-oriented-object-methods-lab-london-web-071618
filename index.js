85c42aa4842c45729485268b4d9c8b56751fc5bd
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-09-02 10:57:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ function BoardMember(name, homeState, training) {
   this.veto = function() {
     "No, I must disagree"
   };
+  this.approve = function() {
+    "You can do that!"
+  };
+  this.doCharity = function() {
+    "I like to help people."
+  };
+  this.releasePressStatement = function() {
+    "You will see great things from Scuber."
+  };
+  this.sayHi = function() {
+    `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
   
 }
 
